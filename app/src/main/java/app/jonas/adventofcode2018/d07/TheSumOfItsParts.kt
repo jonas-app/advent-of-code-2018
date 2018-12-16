72dd51232d103edb0d37f5f5a1bf70ddb039eb64
Jonas <jonas@arkulpa.at>
2018-12-16 01:56:44 +0100

    implement iterable 🔁

--- app/src/main/java/app/jonas/adventofcode2018/d07/TheSumOfItsParts.kt
+++ app/src/main/java/app/jonas/adventofcode2018/d07/TheSumOfItsParts.kt
@@ -4,7 +4,21 @@ import java.io.File
 
 val pattern = Regex("Step ([A-Z]) must be finished before step ([A-Z]) can begin\\.")
 
-class Instructions(file: File) : Iterator<Char> {
+class InstructionsIterator(private var steps: MutableMap<Char, MutableSet<Char>>) : Iterator<Char> {
+    override fun hasNext() = steps.isNotEmpty()
+
+    override fun next(): Char {
+        val prerequisitesDone = steps.filter { it.value.isEmpty() }.toSortedMap()
+        val step = prerequisitesDone.firstKey()
+        steps.remove(step)
+        steps.forEach { it.value.remove(step) }
+        return step
+    }
+}
+
+class Instructions(file: File) : Iterable<Char> {
+    override fun iterator() = InstructionsIterator(steps.toMutableMap())
+
     private val steps = file.readLines().fold(mutableMapOf<Char, MutableSet<Char>>()) { steps, line ->
         pattern.matchEntire(line)?.groupValues?.drop(1)?.map(String::single)?.let { (prerequisite, step) ->
             if (steps[prerequisite] == null) {
@@ -18,23 +32,9 @@ class Instructions(file: File) : Iterator<Char> {
         }
         steps
     }
-
-    override fun hasNext() = steps.isNotEmpty()
-
-    override fun next(): Char {
-        val prerequisitesDone = steps.filter { it.value.isEmpty() }.toSortedMap()
-        val step = prerequisitesDone.firstKey()
-        steps.remove(step)
-        steps.forEach { it.value.remove(step) }
-        return step
-    }
 }
 
-fun part1(file: File): String {
-    var orderedSteps = ""
-    Instructions(file).forEach { orderedSteps += it }
-    return orderedSteps
-}
+fun part1(file: File) = Instructions(file).fold("") { acc, step -> acc + step }
 
 fun part2(file: File): Int {
     return 1
