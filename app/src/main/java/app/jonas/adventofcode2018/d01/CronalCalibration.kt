cee1b3566539bdc13ff2050757c93ec675cc4410
Jonas <jonas@arkulpa.at>
2018-12-18 20:05:50 +0100

    boost performance of day 1️⃣ part 👍 by using a set 🚀

--- app/src/main/java/app/jonas/adventofcode2018/d01/CronalCalibration.kt
+++ app/src/main/java/app/jonas/adventofcode2018/d01/CronalCalibration.kt
@@ -5,7 +5,7 @@ import java.io.File
 fun part1(file: File) = file.readLines().fold(0) { acc, s -> acc + s.toInt() }
 
 fun part2(file: File): Int {
-    val prev = mutableListOf<Int>()
+    val prev = mutableSetOf<Int>()
     var res = 0
     var i = 0
     val input = file.readLines()
